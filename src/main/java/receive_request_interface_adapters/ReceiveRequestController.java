2944a7995f5bad9edea32241f36e57366be2b67d
ykhanghan <353458821@educ.dpcdsb.org>
2022-11-12 04:22:44 -0500

    Added packages and some use case classes

---
+++ src/main/java/receive_request_interface_adapters/ReceiveRequestController.java
@@ -0,0 +1,10 @@
+package receive_request_interface_adapters;
+
+import receive_request_use_case.ReceiveRequestInputBoundary;
+
+public class ReceiveRequestController {
+
+    public ReceiveRequestController(ReceiveRequestInputBoundary a) {
+
+    }
+}
