f10f5621d603f09969f219989beb2e59e219e7c7
ykhanghan <353458821@educ.dpcdsb.org>
2022-12-08 00:17:18 -0500

    Commentate ReceiveRequestFrame

--- src/main/java/receive_request/receive_request_interface_adapters/ReceiveRequestPresenter.java
+++ src/main/java/receive_request/receive_request_interface_adapters/ReceiveRequestPresenter.java
@@ -4,6 +4,9 @@ import receive_request.receive_request_use_case.ReceiveRequestOutputBoundary;
 import receive_request.receive_request_use_case.ReceiveRequestOutputModel;
 
 public class ReceiveRequestPresenter implements ReceiveRequestOutputBoundary {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ReceiveRequestOutputModel result(ReceiveRequestOutputModel receiveRequestOutputModel) {
         return receiveRequestOutputModel;
--- src/main/java/receive_request/receive_request_use_case/ReceiveRequestOutputBoundary.java
+++ src/main/java/receive_request/receive_request_use_case/ReceiveRequestOutputBoundary.java
@@ -1,5 +1,8 @@
 package receive_request.receive_request_use_case;
 
 public interface ReceiveRequestOutputBoundary{
+    /**
+     * Return the Output Model
+     */
     ReceiveRequestOutputModel result(ReceiveRequestOutputModel receiveRequestOutputModel);
 }
