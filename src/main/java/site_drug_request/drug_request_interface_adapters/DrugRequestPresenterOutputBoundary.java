992e9b27099eeb275309b42d93a562639b71dfbd
prospecops <tahasyed1@live.ca>
2022-12-08 18:01:07 -0500

    Changes by khang

--- src/main/java/site_drug_request/drug_request_interface_adapters/DrugRequestPresenterOutputBoundary.java
+++ src/main/java/site_drug_request/drug_request_interface_adapters/DrugRequestPresenterOutputBoundary.java
@@ -5,7 +5,4 @@ import site_drug_request.drug_request_use_case.DrugRequestResponseModel;
 public interface DrugRequestPresenterOutputBoundary {
 
     DrugRequestResponseModel viewPresenter (DrugRequestResponseModel drugRequestResponseModel);
-//    void drugRequestSubmitted();
-//    void practiceDrugRequestSubmitted();
-//    void actionPerformed (ActionEvent actionEvent);
 }
