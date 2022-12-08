71a6ea8f4a95477e478c8c8adf5003ae6ba23408
joshua cuevas <joshua.cuevas@mail.utoronto.ca>
2022-12-08 10:39:04 -0500

    Drug Fulfill Test Cases 0.6

--- src/main/java/drug_fulfill/drug_fulfill_use_case/DrugFulfillResponseModel.java
+++ src/main/java/drug_fulfill/drug_fulfill_use_case/DrugFulfillResponseModel.java
@@ -2,42 +2,49 @@ package drug_fulfill.drug_fulfill_use_case;
 
 // Use case layer
 
+import java.time.LocalDateTime;
+
 public class DrugFulfillResponseModel {
 
-    String drugName;
-    String creationTime;
+    private final String drugName;
+    private int drugBottle;
+    private String creationTime;
+    private final Boolean isEmergency;
+    private final int IDNumber;
+    private final String depotName;
 
-    Boolean isEmergency;
 
 
-    public DrugFulfillResponseModel(String drugName, String creationTime, Boolean isEmergency) {
+    public DrugFulfillResponseModel(String drugName, String creationTime, Boolean isEmergency, int ID, String depot) {
         this.drugName = drugName;
         this.creationTime = creationTime;
         this.isEmergency = isEmergency;
+        this.IDNumber = ID;
+        this.depotName = depot;
     }
 
     public String getDrugName() {
         return drugName;
     }
 
-    public void setDrugName(String login) {
-        this.drugName = login;
-    }
-
     public String getCreationTime() {
         return creationTime;
-    }
-
-    public void setCreationTime(String creationTime) {
-        this.creationTime = creationTime;
     }
 
     public Boolean getIsEmergency() {
         return isEmergency;
     }
 
-    public void setIsEmergency(Boolean isEmergency) {
-        this.isEmergency = isEmergency;
+    public int getIDNumber(){
+        return IDNumber;
     }
-
+    public String getDepotName(){
+        return depotName;
+    }
+    public int getDrugBottle(){
+        return drugBottle;
+    }
+    public void setCreationTime(String creationTime){
+        this.creationTime = creationTime;
+    }
 }
