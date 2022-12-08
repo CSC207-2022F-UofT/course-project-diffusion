f9d0049e242105d9203dcee6b7e040bc8c8d404e
prospecops <tahasyed1@live.ca>
2022-12-08 16:30:24 -0500

    Changes by khang

--- src/main/java/receive_request/receive_request_use_case/ReceiveRequestDatabaseAccessorInterface.java
+++ src/main/java/receive_request/receive_request_use_case/ReceiveRequestDatabaseAccessorInterface.java
@@ -9,5 +9,5 @@ public interface ReceiveRequestDatabaseAccessorInterface {
      * @param drugName is the name of the drug
      * @param drugBottle is the quantity
      */
-    boolean checkInventory(String drugName, String drugBottle) throws FileNotFoundException;
+    String checkInventory(String drugName, String drugBottle) throws FileNotFoundException;
 }
